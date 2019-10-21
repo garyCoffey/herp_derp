d682ad45a61fb8f86b598bb1b8018a1710f841e1
Ramya Kannan <rkannan@enova.com>
2019-10-21 15:48:47 -0500

    render json

--- app/controllers/site_input_controller.rb
+++ app/controllers/site_input_controller.rb
@@ -3,7 +3,7 @@ class SiteInputController < ApplicationController
   def project_description
     project = params[:user_input]
     if project.blank?
-      render "Please describe your project in plain english, using words.", :status => 200
+      render :json => "Please describe your project in plain english, using words.", :status => 200
       return
     end
     ##String processing - what are we processing though?
